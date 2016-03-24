1a4e604463789e4e00448131f3d62e7e5e95a73a
YeXiaoChao <yechao@ycdoit.com>
2016-03-24 17:23:26 +0800

    update

---
+++ js/example/music_player.js
@@ -0,0 +1,17 @@
+var musicList = [
+    {
+        title : 'You’re Beautiful',
+        singer : 'Various Artists',
+        cover  : 'http://yexiaochao.github.io/music/Various%20Artists/So%20Beautiful%201/433125.jpg',
+        src    : 'https://yexiaochao.github.io/music/Various Artists/So Beautiful 1/01 - You’re Beautiful.mp3'
+    },
+    {
+        title : 'E.T',
+        singer : 'Katy Perry',
+        cover  : 'http://yexiaochao.github.io/music/Katy%20Perry/Teenage%20Dream/disc%2001/396236.jpg',
+        src    : 'https://yexiaochao.github.io/music/Katy Perry/Teenage Dream/disc 01/08 - E.T.mp3'
+    }
+];
+new SMusic({
+    musicList:musicList
+});
