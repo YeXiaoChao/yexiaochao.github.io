1b2051fde5f00eab7e2de484d23862ebe06cc06c
YeXiaoChao <yechao@ycdoit.com>
2016-07-11 19:49:57 +0800

    update

---
+++ example/js/page4top.js
@@ -0,0 +1,53 @@
+ window.onload = function(){
+    var btnTop = document.getElementById("btnTop");
+    var timer = null;
+
+    window.onscroll = function(){
+        var backTop = getScrollTop();
+        if(backTop >= 20){
+            btnTop.style.display = "block";
+        }else {
+            btnTop.style.display = "none";
+        }
+    };
+
+    btnTop.onclick = function(){
+        //定时执行
+        timer = setInterval(function(){
+            var backTop = getScrollTop();
+            var speedTop = backTop / 10;
+            setScrollTop(backTop - speedTop);
+            if(backTop == 0){
+                //结束函数执行
+                clearInterval(timer);
+            }
+        },30);
+    };
+
+    var getScrollTop = function(){
+        var sTop ;
+        if (document.compatMode == "BackCompat")
+        {
+            sTop = document.body.scrollTop;
+        }
+        else
+        {
+            //document.compatMode == \"CSS1Compat\"
+            sTop = document.documentElement.scrollTop == 0 ? document.body.scrollTop : document.documentElement.scrollTop;
+        }
+        return sTop;
+    };
+
+    var setScrollTop = function(top){
+
+        if (document.compatMode == "BackCompat")
+        {
+           document.body.scrollTop = top;
+        }
+        else
+        {
+            document.body.scrollTop = top;
+            document.documentElement.scrollTop = top;
+        }
+    }
+};
