89b3a5641ab1169cdc7e158405a26c5599cb47a6
YeXiaoChao <yechao@ycdoit.com>
2016-07-23 10:31:40 +0800

    update

---
+++ js/outer_line/tongji4blogluka.js
@@ -0,0 +1,9 @@
+<script>
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?c843442eb017fd5b606f4587aa9be5c7";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
+</script>
