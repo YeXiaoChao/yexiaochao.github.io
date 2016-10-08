18fe3c2e7db5d06768cc018426bd84df0b066ef4
YeXiaoChao <yechao@ycdoit.com>
2016-10-08 11:57:40 +0800

    update

--- js/base.js
+++ js/base.js
@@ -1,2 +1,10 @@
 var windowHeight =  window.innerHeight;
 document.getElementById("content").style.minHeight =( windowHeight-99 )+"px";
+//百度统计
+var _hmt = _hmt || [];
+(function() {
+    var hm = document.createElement("script");
+    hm.src = "//hm.baidu.com/hm.js?bd44011799a85274b9e61214e4f211ae";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+})();
--- js/outer_line/tongji4blogluka.js
+++ js/outer_line/tongji4blogluka.js
@@ -1,9 +1,7 @@
-<script>
 var _hmt = _hmt || [];
 (function() {
     var hm = document.createElement("script");
-    hm.src = "//hm.baidu.com/hm.js?c843442eb017fd5b606f4587aa9be5c7";
+    hm.src = "//hm.baidu.com/hm.js?bd44011799a85274b9e61214e4f211ae";
     var s = document.getElementsByTagName("script")[0];
     s.parentNode.insertBefore(hm, s);
 })();
-</script>
